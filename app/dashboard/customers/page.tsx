04e22032dbb888032812d246dd14d43e8dbacd86
David <atembarry12@gmail.com>
2024-11-28 11:10:31 +0100

    Pushing to Github, Vercel

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page(){
+    return <p className="text-lg font-bold">Customers Page</p>
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import {Inter, Lusitana} from 'next/font/google'
+import { Indie_Flower } from 'next/font/google'
+
+
+export const inter = Inter({subsets: ["latin"]})
+export const lusitana = Lusitana({weight: '400', subsets:['latin']})
+export const indie = Indie_Flower({subsets: ['latin'], weight: '400'})
