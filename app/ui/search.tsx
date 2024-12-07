a6de1f84568615fdef0eab04ca2706bd3c7f209d
David <atembarry12@gmail.com>
2024-12-07 23:08:28 +0100

    Chapter 11

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,24 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const {replace} = useRouter()
+
+  function handleChange(term: string){
+    const urlSearch = new URLSearchParams(searchParams)
+
+    if (term) {
+      urlSearch.set('query', term);
+    } else {
+      urlSearch.delete('query');
+    }
+
+    replace(`${pathname}?${urlSearch.toString()}`);
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,7 +27,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleChange(e.target.value)
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
+      
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
